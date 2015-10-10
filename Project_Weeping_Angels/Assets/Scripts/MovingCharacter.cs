be201a45002ba42cb762023d972927494fc77ddc
shgfl007 <shgfl@hotmail.com>
2015-10-09 21:33:07 -0400

    Update
    Walking Gesture Added!
    Needs modifications tho…
    Plz save a new scene when you using the environment scene!!

--- Project_Weeping_Angels/Assets/Scripts/MovingCharacter.cs
+++ Project_Weeping_Angels/Assets/Scripts/MovingCharacter.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class MovingCharacter : MonoBehaviour {
 
-	// Use this for initialization
+	private GameObject player; 
+
+	private Vector3 direction;
+	private bool isMoving = false;
+	public float speed;
+
 	void Start () {
-	
+		player = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
-	
+		isMoving = WalkingGesture.isMoving;
+		if (isMoving) {
+			direction = WalkingGesture.moving_Direction;
+			_moveCharacter();
+		}
+	}
+
+	void _moveCharacter(){
+		//speed is in m/s
+		Debug.Log ("moving character function called");
+		//float speed = 30.0f;
+		CharacterController controller = player.GetComponent<CharacterController> ();
+		//transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+		Vector3 forward = transform.TransformDirection(direction);
+		float curSpeed = speed * Input.GetAxis("Vertical");
+		//controller.SimpleMove(forward * curSpeed);
+		direction.x = -direction.x;
+		direction.y = -direction.y;
+		direction.z = -direction.z;
+		controller.Move (direction * Time.deltaTime);
+
 	}
 }
--- Project_Weeping_Angels/Assets/Scripts/WalkingGesture.cs
+++ Project_Weeping_Angels/Assets/Scripts/WalkingGesture.cs
@@ -8,6 +8,12 @@ public class WalkingGesture : MonoBehaviour {
 
 	public bool isLeftHanded = false;
 
+	public static Vector Direction = Vector.Zero;
+
+	public float Z_THRESHOLD_MIN = -0.4f;
+	public float Z_THRESHOLD_MAX = -0.2f;
+	public static bool isMoving = false;
+	public static Vector3 moving_Direction = Vector3.zero;
 	void Start () {
 		_controller = new Controller ();
 	}
@@ -34,31 +40,37 @@ public class WalkingGesture : MonoBehaviour {
 		}
 
 		//using right hand as the controller for the player
-		FingerList fingers = rightHand.Fingers;
-		Finger Index_Finger = new Finger();
-		for (int i = 0; i < fingers.Count; i++) {
-			Debug.Log(fingers[i].Type);
-			if(fingers[i].Type.Equals(Finger.FingerType.TYPE_INDEX))
-			{
-				Debug.Log("index finger found!");
-				Index_Finger = fingers[i];
+		if (rightHand != null) {
+			FingerList fingers = rightHand.Fingers;
+			Finger Index_Finger = new Finger ();
+			for (int i = 0; i < fingers.Count; i++) {
+				//Debug.Log(fingers[i].Type);
+				if (fingers [i].Type.Equals (Finger.FingerType.TYPE_INDEX)) {
+					//Debug.Log("index finger found!");
+					Index_Finger = fingers [i];
+				}
 			}
+			//finger index: 
+			//TYPE_THUMB = = 0 -
+//			TYPE_INDEX = = 1 -
+//			TYPE_MIDDLE = = 2 -
+//			TYPE_RING = = 3 -
+//			TYPE_PINKY = = 4 - 
+
+
+
+			//store the middle finger
+			Finger Middle_Finger = rightHand.Finger (2);
+
+			//the walking gesture: index finger pointing forward
+			if (Index_Finger.IsValid) {
+				//Debug.Log ("index finger direction is " + Index_Finger.Direction.ToString ());
+				isMoving = true;
+				moving_Direction = Index_Finger.Direction.ToUnity ();
+			} else
+				isMoving = false;
 		}
-		//finger index: 
-		//TYPE_THUMB = = 0 -
-//		TYPE_INDEX = = 1 -
-//		TYPE_MIDDLE = = 2 -
-//		TYPE_RING = = 3 -
-//		TYPE_PINKY = = 4 - 
 
 
-
-		//store the middle finger
-		Finger Middle_Finger = rightHand.Finger (2);
-
-		//the walking gesture: index finger pointing forward
-		if(Index_Finger.IsValid)
-		Debug.Log ("index finger direction is " + Index_Finger.Direction.ToString ());
-	
 	}
 }
